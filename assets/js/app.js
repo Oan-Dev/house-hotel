f559d2d2da2c733f491b08f07fa4abff7f8070c2
OAN <oan@fedora.home>
2023-01-06 18:54:03 +0100

    feat : hamburger and none navbar display for respovive mobile. bug : no return initial position

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,15 @@ console.log(header);
 onscroll = () => {
     header.classList.toggle('active', window.scrollY > 0);
 }
+const hamburger = document.querySelector(".hamburger"); 
+const nav = document.querySelector("nav"); 
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle('active');
+    nav.classList.toggle('active');
+})
+
+document.querySelectorAll(" nav a").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove('active');
+    nav.classList.remove('active');
+} ))
